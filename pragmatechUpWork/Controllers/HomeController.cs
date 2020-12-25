69a1647cb2928363adae38fdaa6e6e89175a420c
Garibov2001 <mahmood.garibov@gmail.com>
2020-12-26 01:34:34 +0400

    Layout is done.

---
+++ pragmatechUpWork/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pragmatechUpWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/")]
+        public IActionResult Home()
+        {
+            return View("home");
+        }
+
+        [Route("/projects")]
+        public IActionResult Projects()
+        {
+            return View("projects");
+        }
+
+
+    }
+}
